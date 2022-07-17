2768f42bdca1fbba6b3647f5d86e64168ccfb2f9
Brian Gicharu <brian.gicharu@gmail.com>
2022-07-17 18:57:13 +0300

    Initialized testNG.xml

--- src/functionalTests/testsByBrian/tests/Cart_Operations.java
+++ src/functionalTests/testsByBrian/tests/Cart_Operations.java
@@ -1,5 +1,7 @@
 package functionalTests.testsByBrian.tests;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
